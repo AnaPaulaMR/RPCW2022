24998fcc54febf4b2f1d89f10045b3aa9f9b4b1f
Ana Paula <38854926+AnaPaulaMR@users.noreply.github.com>
2022-04-17 22:27:23 +0100

    TPC7 feito :)

---
+++ TP7/TPC7/routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var axios = require('axios');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, ) {
+  axios.get('http://clav-api.di.uminho.pt/v2/classes?nivel=1&apikey=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNGNiYTg0OWJhYmI2NjdjYmZkYzE2ZSIsImlhdCI6MTY0OTE5NTY1MiwiZXhwIjoxNjUxNzg3NjUyfQ.EuvH713Qr6IZ073-5FMF6j5p_3tb6Trv0TOOF5ZHWOPUlCBqKU1H9DTo_ueoCyWhPbEd6F8xzNvn-UkG3J8Ppq65xF8uukoElnSIsi3kldXI2E_EHMv5ETIq-2SGpiBmLyv1zu2broi-nXw18XwKM-WWpoumw5mZacg1qyj4kokGm--WzPIDD15Uibu2ObsDfeHpbDt81Npq-WgEVe56F5w0TdAvY_b-Xvm77hXI4MuaatL9bsOtYEyiepLuBelDyVWjAIoon3-7tB1lwrPnC0OJ_cxKUyCdqx8sZPkmciyTmBsV8fDTyvTP1ibiryAQsDRK5TrG83CcWmStZyDnoQ')
+    .then(response => {
+      var list = response.data;
+      res.render('index', { data: list });
+    })
+    .catch(function (err) {
+      res.render('error', { error: err });
+    });
+});
+
+router.get('/classe/:id', function(req, res, ) {
+  axios.get('http://clav-api.di.uminho.pt/v2/classes/c' + req.params.id + '?apikey=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNGNiYTg0OWJhYmI2NjdjYmZkYzE2ZSIsImlhdCI6MTY0OTE5NTY1MiwiZXhwIjoxNjUxNzg3NjUyfQ.EuvH713Qr6IZ073-5FMF6j5p_3tb6Trv0TOOF5ZHWOPUlCBqKU1H9DTo_ueoCyWhPbEd6F8xzNvn-UkG3J8Ppq65xF8uukoElnSIsi3kldXI2E_EHMv5ETIq-2SGpiBmLyv1zu2broi-nXw18XwKM-WWpoumw5mZacg1qyj4kokGm--WzPIDD15Uibu2ObsDfeHpbDt81Npq-WgEVe56F5w0TdAvY_b-Xvm77hXI4MuaatL9bsOtYEyiepLuBelDyVWjAIoon3-7tB1lwrPnC0OJ_cxKUyCdqx8sZPkmciyTmBsV8fDTyvTP1ibiryAQsDRK5TrG83CcWmStZyDnoQ')
+    .then(response => {
+      var data = response.data;
+
+      axios.get('http://clav-api.di.uminho.pt/v2/classes/c' + req.params.id + '/procRel?apikey=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNGNiYTg0OWJhYmI2NjdjYmZkYzE2ZSIsImlhdCI6MTY0OTE5NTY1MiwiZXhwIjoxNjUxNzg3NjUyfQ.EuvH713Qr6IZ073-5FMF6j5p_3tb6Trv0TOOF5ZHWOPUlCBqKU1H9DTo_ueoCyWhPbEd6F8xzNvn-UkG3J8Ppq65xF8uukoElnSIsi3kldXI2E_EHMv5ETIq-2SGpiBmLyv1zu2broi-nXw18XwKM-WWpoumw5mZacg1qyj4kokGm--WzPIDD15Uibu2ObsDfeHpbDt81Npq-WgEVe56F5w0TdAvY_b-Xvm77hXI4MuaatL9bsOtYEyiepLuBelDyVWjAIoon3-7tB1lwrPnC0OJ_cxKUyCdqx8sZPkmciyTmBsV8fDTyvTP1ibiryAQsDRK5TrG83CcWmStZyDnoQ')
+      .then(response => {
+        var rel = response.data,
+            lrel = ['eCruzadoCom', 'eComplementarDe', 'eSuplementoDe', 'eSuplementoPara'];
+        
+        var filteredArray = rel.filter(function(itm){
+          return lrel.indexOf(itm.idRel) > -1;
+        });
+
+        res.render('classe', { data: data, rel: filteredArray});
+      })
+      .catch(function (err) {
+        res.render('error', { error: err });
+      });
+    })
+    .catch(function (err) {
+      res.render('error', { error: err });
+    });
+});
+module.exports = router;
